ea523794416e0783af74a51ca669bb22a8420cda
Anas SAHEL <anas.sahel@gmail.com>
2023-07-03 13:12:24 +0000

    feat(webpart/welcome-user): Create the webpart
    - Create a welcome user webpart

--- src/webparts/welcomeUser/WelcomeUserWebPart.tsx
+++ src/webparts/welcomeUser/WelcomeUserWebPart.tsx
@@ -1,80 +1,25 @@
-import { IReadonlyTheme } from '@microsoft/sp-component-base';
 import { Version } from '@microsoft/sp-core-library';
-import {
-  IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 
-import * as strings from 'WelcomeUserWebPartStrings';
 import WelcomeUser from './components/WelcomeUser';
 
+import { IPropertyPaneConfiguration, PropertyPaneTextField } from '@microsoft/sp-property-pane';
 import "../../main.css";
 
-export interface IWelcomeUserWebPartProps {
-  description: string;
+export type WelcomeUserWebPartProps = {
+  welcomeMessage: string;
 }
 
-export default class WelcomeUserWebPart extends BaseClientSideWebPart<IWelcomeUserWebPartProps> {
+export default class WelcomeUserWebPart extends BaseClientSideWebPart<WelcomeUserWebPartProps> {
 
   public render(): void {
-
-
-    ReactDom.render(<WelcomeUser userDisplayName={this.context.pageContext.user.displayName} />, this.domElement);
+    ReactDom.render(<WelcomeUser welcomeMessage={this.properties.welcomeMessage} userDisplayName={this.context.pageContext.user.displayName} />, this.domElement);
   }
 
   protected onInit(): Promise<void> {
-    return this._getEnvironmentMessage().then(message => {
-      return;
-    });
-  }
-
-
-
-  private _getEnvironmentMessage(): Promise<string> {
-    if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
-      return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
-        .then(context => {
-          let environmentMessage: string = '';
-          switch (context.app.host.name) {
-            case 'Office': // running in Office
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
-              break;
-            case 'Outlook': // running in Outlook
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
-              break;
-            case 'Teams': // running in Teams
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
-              break;
-            default:
-              throw new Error('Unknown host');
-          }
-
-          return environmentMessage;
-        });
-    }
-
-    return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
-  }
-
-  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
-    if (!currentTheme) {
-      return;
-    }
-
-
-    const {
-      semanticColors
-    } = currentTheme;
-
-    if (semanticColors) {
-      this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
-      this.domElement.style.setProperty('--link', semanticColors.link || null);
-      this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
-    }
-
+    return super.onInit();
   }
 
   protected onDispose(): void {
@@ -90,20 +35,19 @@ export default class WelcomeUserWebPart extends BaseClientSideWebPart<IWelcomeUs
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: "Welcome webpart"
           },
           groups: [
             {
-              groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
+                PropertyPaneTextField('welcomeMessage', {
+                  label: 'Welcome Message'
                 })
               ]
             }
           ]
         }
       ]
-    };
+    }
   }
 }
--- src/webparts/welcomeUser/components/WelcomeUser.tsx
+++ src/webparts/welcomeUser/components/WelcomeUser.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 //import styles from './WelcomeUser.module.scss';
-//import { escape } from '@microsoft/sp-lodash-subset';
+import { escape } from '@microsoft/sp-lodash-subset';
 
-export type WelcomeUserProps = { userDisplayName: string; }
+export type WelcomeUserProps = { welcomeMessage?: string; userDisplayName: string; }
 
-const WelcomeUser: React.FC<WelcomeUserProps> = ({ userDisplayName }) => {
-  return <section className="text-primary-600 font-bold">Welcome {userDisplayName}</section>
+const WelcomeUser: React.FC<WelcomeUserProps> = ({ welcomeMessage = "Welcome, ", userDisplayName }) => {
+  return <section className="shadow p-4 rounded border border-primary-500/50 text-xl text-center bg-primary-50">{escape(welcomeMessage)}
+    <span className="font-bold text-primary-700">
+      {escape(userDisplayName)}
+    </span>
+  </section>
 }
 
 export default WelcomeUser;
